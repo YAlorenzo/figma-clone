9a8362d67a9fb09af6c347445b84b9fbbd8666c4
YAlorenzo <zhenia.klimenchuk@gmail.com>
2024-07-25 21:25:08 +0200

    start

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Room } from "./room";
+import { CollaborativeApp } from "./CollaborativeApp";
+
+export default function Page() {
+  return (
+    <Room>
+      <CollaborativeApp />
+    </Room>
+  );
+}
